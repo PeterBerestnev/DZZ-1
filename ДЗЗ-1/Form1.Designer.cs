4d7aa8624ada57cbf0f589bc0e5f150bb8a23b43
PeterBerestnev <Petkadrifter@gmail.com>
2024-10-06 18:25:53 +0300

    Добавлено масштабирование

--- ДЗЗ-1/Form1.Designer.cs
+++ ДЗЗ-1/Form1.Designer.cs
@@ -47,10 +47,19 @@
             vScrollBar1 = new VScrollBar();
             openFileDialog1 = new OpenFileDialog();
             listView1 = new ListView();
+            button1 = new Button();
+            groupBox4 = new GroupBox();
+            checkBox2 = new CheckBox();
+            checkBox1 = new CheckBox();
+            groupBox5 = new GroupBox();
+            label4 = new Label();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)graphics).BeginInit();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             groupBox3.SuspendLayout();
+            groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
             SuspendLayout();
             // 
             // graphics
@@ -61,6 +70,7 @@
             graphics.Size = new Size(500, 561);
             graphics.TabIndex = 0;
             graphics.TabStop = false;
+            graphics.MouseClick += pictureBox1_MouseClick;
             graphics.MouseMove += pictureBox1_MouseMove;
             // 
             // groupBox1
@@ -99,9 +109,9 @@
             groupBox2.Controls.Add(radioButton3);
             groupBox2.Controls.Add(radioButton2);
             groupBox2.Controls.Add(radioButton1);
-            groupBox2.Location = new Point(533, 88);
+            groupBox2.Location = new Point(12, 22);
             groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(331, 57);
+            groupBox2.Size = new Size(180, 54);
             groupBox2.TabIndex = 2;
             groupBox2.TabStop = false;
             groupBox2.Text = "Сдвигать коды на:";
@@ -161,30 +171,30 @@
             groupBox3.Controls.Add(label6);
             groupBox3.Controls.Add(label5);
             groupBox3.Controls.Add(label3);
-            groupBox3.Location = new Point(533, 151);
+            groupBox3.Location = new Point(533, 253);
             groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(216, 106);
+            groupBox3.Size = new Size(156, 106);
             groupBox3.TabIndex = 3;
             groupBox3.TabStop = false;
             groupBox3.Text = "Координаты курсора:";
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(134, 72);
+            textBox4.Location = new Point(87, 72);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(60, 23);
             textBox4.TabIndex = 7;
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(134, 46);
+            textBox3.Location = new Point(87, 46);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(60, 23);
             textBox3.TabIndex = 6;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(134, 19);
+            textBox1.Location = new Point(87, 19);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(60, 23);
             textBox1.TabIndex = 4;
@@ -239,15 +249,89 @@
             listView1.TabIndex = 5;
             listView1.UseCompatibleStateImageBehavior = false;
             // 
+            // button1
+            // 
+            button1.Location = new Point(12, 125);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Сброс";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(checkBox2);
+            groupBox4.Controls.Add(checkBox1);
+            groupBox4.Controls.Add(groupBox5);
+            groupBox4.Controls.Add(button1);
+            groupBox4.Controls.Add(groupBox2);
+            groupBox4.Location = new Point(533, 88);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(331, 159);
+            groupBox4.TabIndex = 7;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "Модификации";
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(13, 101);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(105, 19);
+            checkBox2.TabIndex = 9;
+            checkBox2.Text = "Нормировать";
+            checkBox2.UseVisualStyleBackColor = true;
+            checkBox2.CheckStateChanged += checkBox2_CheckStateChanged;
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(13, 82);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(129, 19);
+            checkBox1.TabIndex = 8;
+            checkBox1.Text = "Интерполировать";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckStateChanged += checkBox1_CheckStateChanged;
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(label4);
+            groupBox5.Controls.Add(label2);
+            groupBox5.Location = new Point(200, 31);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(125, 45);
+            groupBox5.TabIndex = 7;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "Приближение";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(18, 18);
+            label4.Name = "label4";
+            label4.Size = new Size(0, 15);
+            label4.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(5, 18);
+            label2.Name = "label2";
+            label2.Size = new Size(21, 15);
+            label2.TabIndex = 0;
+            label2.Text = "x1";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(872, 607);
+            Controls.Add(groupBox4);
             Controls.Add(listView1);
             Controls.Add(vScrollBar1);
             Controls.Add(groupBox3);
-            Controls.Add(groupBox2);
             Controls.Add(groupBox1);
             Controls.Add(graphics);
             Name = "Form1";
@@ -259,6 +343,10 @@
             groupBox2.PerformLayout();
             groupBox3.ResumeLayout(false);
             groupBox3.PerformLayout();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            groupBox5.PerformLayout();
             ResumeLayout(false);
         }
 
@@ -283,5 +371,12 @@
         private VScrollBar vScrollBar1;
         private OpenFileDialog openFileDialog1;
         private ListView listView1;
+        private Button button1;
+        private GroupBox groupBox4;
+        private GroupBox groupBox5;
+        private Label label4;
+        private Label label2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
     }
 }
--- ДЗЗ-1/Form1.cs
+++ ДЗЗ-1/Form1.cs
@@ -7,9 +7,15 @@ namespace ДЗЗ_1
     {
         private byte[] _fileData;
         private int shift = 0;
-        private int topPixel;
+        private int scrollPosition;
+        private int zoomVarX = 0;
+        private int zoomVarY = 0;
         private int width;
         private int height;
+        private int zoomLevel = 1;
+        private int minBrightness;
+        private int maxBrightness;
+        private Bitmap originalImage;
         public Form1()
         {
             InitializeComponent();
@@ -24,15 +30,35 @@ namespace ДЗЗ_1
                     _fileData = File.ReadAllBytes(openFileDialog1.FileName);
                     width = BitConverter.ToInt16(_fileData, 0);
                     height = BitConverter.ToInt16(_fileData, 2);
-                    drawImage(_fileData);
 
                     listView1.Columns.Add("Файл");
                     ListViewItem item = new ListViewItem(openFileDialog1.FileName);
                     listView1.Items.Add(item);
 
-                    // Установите ширину первой колонки на всю ширину ListView
                     listView1.Columns[0].Width = (int)(listView1.Width * 1);
                     vScrollBar1.Maximum = height - graphics.Height;
+                    for (int y = 0; y < height; y++)
+                    {
+                        for (int x = 0; x < width; x++)
+                        {
+                            int pixelOffset = 4 + (y * width * 2) + (x * 2);
+                            ushort pixelValue = BitConverter.ToUInt16(_fileData, pixelOffset);
+                            int currentBrightness = (pixelValue & 0x3FF);
+
+                            if (currentBrightness < minBrightness)
+                            {
+                                minBrightness = currentBrightness;
+                            }
+
+                            if (currentBrightness > maxBrightness)
+                            {
+                                maxBrightness = currentBrightness;
+                            }
+                        }
+                    }
+
+                    resetParams();
+                  
                 }
                 catch (Exception ex)
                 {
@@ -42,27 +68,18 @@ namespace ДЗЗ_1
             }
         }
 
-        private void drawImage(byte[] image)
+        private void resetParams()
         {
-            Bitmap bitmap = new Bitmap(width, height);
-
-            for (int y = topPixel; y < height && y < topPixel + bitmap.Height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    int pixelOffset = 4 + (y * width * 2) + (x * 2);
-                    ushort pixelValue = BitConverter.ToUInt16(image, pixelOffset);
-
-                    int brightness = (pixelValue & 0x3FF);
-
-                    int scaledBrightness = (brightness >> shift) & 0xFF;
-
-                    Color pixelColor = Color.FromArgb(scaledBrightness, scaledBrightness, scaledBrightness);
-                    bitmap.SetPixel(x, y - topPixel, pixelColor);
-                }
-            }
-
-            graphics.Image = bitmap;
+            //Reset Params
+            shift = 0;
+            scrollPosition = 0;
+            vScrollBar1.Value = 0;
+            zoomLevel = 1;
+            label2.Text = $"x{zoomLevel}";
+            zoomVarY = 0;
+            zoomVarX = 0;
+            checkBox1.Checked = false;
+            drawImage(_fileData);
         }
 
         private void radioButton_CheckedChanged(object sender, EventArgs e)
@@ -80,11 +97,11 @@ namespace ДЗЗ_1
             if (graphics.Image != null)
             {
                 int x = e.X;
-                int y = e.Y + topPixel;
+                int y = e.Y + scrollPosition;
 
                 Bitmap bitmap = (Bitmap)graphics.Image;
 
-                if (x >= 0 && x < bitmap.Width && y >= 0 && y < bitmap.Height + topPixel)
+                if (x >= 0 && x < bitmap.Width && y >= 0 && y < bitmap.Height + scrollPosition)
                 {
                     Color pixelColor = bitmap.GetPixel(x, y);
 
@@ -100,15 +117,141 @@ namespace ДЗЗ_1
             }
         }
 
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (graphics.Image != null)
+            {
+                int x = e.X + zoomVarX;
+                int y = e.Y + scrollPosition + zoomVarY;
+
+                Bitmap bitmap = (Bitmap)graphics.Image;
+
+                if (x >= 0 && x < bitmap.Width && y >= 0 && y < bitmap.Height + scrollPosition)
+                {
+                    // Увеличить изображение
+                    zoomLevel++;
+
+                    // Обновить topPixel и leftPixel в зависимости от центра
+                    zoomVarY = y;
+                    zoomVarX = x;
+                    label2.Text = $"x{zoomLevel}";
+                    drawImage(_fileData);
+                }
+            }
+        }
+
+        private void drawImage(byte[] image)
+        {
+            if (image != null)
+            {
+                // Apply the current zoom level
+                int newWidth = (int)(width * zoomLevel);
+                int newHeight = (int)(height * zoomLevel);
+
+                Bitmap zoomedImage = new Bitmap(newWidth, newHeight);
+
+                for (int yZoom = 0; yZoom < graphics.Height; yZoom++)
+                {
+                    for (int xZoom = 0; xZoom < newWidth; xZoom++)
+                    {
+                        int nearestX = (xZoom + zoomVarX) / zoomLevel;
+                        int nearestY = (yZoom + zoomVarY + scrollPosition) / zoomLevel;
+
+                        // Clamp nearestX and nearestY to the original image bounds
+                        nearestX = Math.Max(0, Math.Min(nearestX, width - 1));
+                        nearestY = Math.Max(0, Math.Min(nearestY, height - 1));
+
+                        int pixelOffset = 4 + (nearestY * width * 2) + (nearestX * 2);
+                        ushort pixelValue = BitConverter.ToUInt16(image, pixelOffset);
+
+                        int brightness = GetBrightness(image, pixelOffset, nearestX, nearestY, xZoom, yZoom);
+
+                        if (checkBox2.Checked)
+                        {
+                            brightness = NormalizeBrightness(brightness);
+                        }
+
+                        int scaledBrightness = (brightness >> shift) & 0xFF;
+
+                        Color pixelColor = Color.FromArgb(scaledBrightness, scaledBrightness, scaledBrightness);
+                        zoomedImage.SetPixel(xZoom, yZoom, pixelColor);
+                    }
+                }
+                originalImage = zoomedImage;
+                graphics.Image = zoomedImage;
+            }
+        }
+
+        private int NormalizeBrightness(int brightness)
+        {
+            return (int)((brightness - minBrightness) / (float)(maxBrightness - minBrightness) * 255);
+        }
+
+        private int GetBrightness(byte[] image, int pixelOffset, int nearestX, int nearestY, int xZoom, int yZoom)
+        {
+            if (checkBox1.Checked)
+            {
+                return GetBilinearInterpolationBrightness(image, nearestX, nearestY, xZoom, yZoom);
+            }
+            else
+            {
+                // Nearest neighbor interpolation
+                return (BitConverter.ToUInt16(image, pixelOffset) & 0x3FF);
+            }
+        }
+
+        private int GetBilinearInterpolationBrightness(byte[] image, int nearestX, int nearestY, int xZoom, int yZoom)
+        {
+            int x0 = Math.Max(0, Math.Min(nearestX, width - 1));
+            int x1 = Math.Max(0, Math.Min(nearestX + 1, width - 1));
+            int y0 = Math.Max(0, Math.Min(nearestY, height - 1));
+            int y1 = Math.Max(0, Math.Min(nearestY + 1, height - 1));
+
+            int pixelOffset00 = 4 + (y0 * width * 2) + (x0 * 2);
+            int pixelOffset10 = 4 + (y0 * width * 2) + (x1 * 2);
+            int pixelOffset01 = 4 + (y1 * width * 2) + (x0 * 2);
+            int pixelOffset11 = 4 + (y1 * width * 2) + (x1 * 2);
+
+            int brightness00 = (BitConverter.ToUInt16(image, pixelOffset00) & 0x3FF);
+            int brightness10 = (BitConverter.ToUInt16(image, pixelOffset10) & 0x3FF);
+            int brightness01 = (BitConverter.ToUInt16(image, pixelOffset01) & 0x3FF);
+            int brightness11 = (BitConverter.ToUInt16(image, pixelOffset11) & 0x3FF);
+
+            float xFraction = (xZoom + zoomVarX) % zoomLevel / (float)zoomLevel;
+            float yFraction = (yZoom + zoomVarY + scrollPosition) % zoomLevel / (float)zoomLevel;
+
+            int brightness = (int)((brightness00 * (1 - xFraction) * (1 - yFraction) +
+                                        brightness10 * xFraction * (1 - yFraction) +
+                                        brightness01 * (1 - xFraction) * yFraction +
+                                        brightness11 * xFraction * yFraction));
+
+            brightness = Math.Max(0, Math.Min(brightness, 255)); // Clamp to 0-255 range
+
+            return brightness;
+        }
+
         private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
         {
             if (graphics.Image != null)
             {
-                topPixel = e.NewValue * (height-graphics.Height) / vScrollBar1.Maximum;
-
+                scrollPosition = e.NewValue * (height - graphics.Height) / vScrollBar1.Maximum;
                 drawImage(_fileData);
             }
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resetParams();
+        }
+
+        private void checkBox1_CheckStateChanged(object sender, EventArgs e)
+        {
+            drawImage(_fileData);
+        }
+
+        private void checkBox2_CheckStateChanged(object sender, EventArgs e)
+        {
+            drawImage(_fileData);
+        }
     }
 }
